f3c4426f608808ca1b70b265938283614e8c0dbd
atikamary <atika@giigi.co>
2017-10-09 20:11:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -12,24 +12,23 @@ function chartData(data) {
   //a quick check to see if we're getting data from the api
   console.log(data)
   console.log (data[0].name)
-  const numberCountries = parseInt(data.length)
-  const countryWidth = 100 / numberCountries
 
   //storing the countries and populations as a new array
-  var countries = data.slice(0, 5).map(function(a) {return a.name;})
-  var population = data.slice(0, 5).map(function(a) {return a.population;})
+  var countries = data.map(function(a) {return a.name;})
+  var population = data.map(function(a) {return a.population;})
+
 
   //let's log the new arrays to see if it worked
   console.log (countries)
   console.log (population)
-}
-  
+
+
 var dataNew = {
   // A labels array that can contain any sort of values
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+  labels: countries,
   // Our series array that contains series objects or in this case series data arrays
   series: [
-    [5, 2, 4, 2, 0]
+    population
   ]
 };
 
@@ -37,7 +36,4 @@ var dataNew = {
 // that is resolving to our chart container element. The Second parameter
 // is the actual data object.
 new Chartist.Line('.ct-chart', dataNew);
-
-
-
-
+}
