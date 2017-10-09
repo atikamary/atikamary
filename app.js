1abd141b81d0778bfbae2cef10b50c0c8dbdf516
atikamary <atika@giigi.co>
2017-10-09 19:48:03 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,27 +4,20 @@ fetch('https://restcountries.eu/rest/v1')
 .then(getJsonResponse)
 .then(chartData)
 
+
 function getJsonResponse(response) {
   return response.json()
 }
 
 function chartData(data) {
-  //a quick check to see if we're getting data from the api
-  console.log(data)
-  console.log (data[0].name)
-  const numberCountries = parseInt(data.length)
-  const countryWidth = 100 / numberCountries
-
-  //storing the countries and populations as a new array
-  var countries = data.slice(0, 5).map(function(a) {return a.name;})
+  return countries = data.slice(0, 5).map(function(a) {return a.name;})
   var population = data.slice(0, 5).map(function(a) {return a.population;})
 
   //let's log the new arrays to see if it worked
   console.log (countries)
   console.log (population)
-}
-
-var newData = {
+  
+  var newData = {
   // A labels array that can contain any sort of values
   labels: countries,
   // Our series array that contains series objects or in this case series data arrays
@@ -32,8 +25,14 @@ var newData = {
     population
   ]
 };
+}
 
 // Create a new line chart object where as first parameter we pass in a selector
 // that is resolving to our chart container element. The Second parameter
 // is the actual data object.
+
 new Chartist.Line('.ct-chart', newData);
+}
+
+
+
